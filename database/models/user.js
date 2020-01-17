4dde5d644546b0d1fdf9d3cdda1a55657f7a226d
Bobur <bobur.hu0991@gmail.com>
2020-01-17 14:37:37 -0500

    user schema

--- database/models/user.js
+++ database/models/user.js
@@ -2,7 +2,26 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const User = db.define("user", {
-    
+    firstName : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phoneNum: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    points : {
+        type: Sequelize.INT,
+        allowNull: false
+    }
 });
 
 module.exports = User;
