c2e32f66ce6d592e264319be2a6d9bb45a652131
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-17 15:45:48 -0500

    created restaurantAddress table fixed #2

--- database/models/restaurantAddress.js
+++ database/models/restaurantAddress.js
@@ -1,4 +1,31 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
+const RestaurantAddress = db.define("restaurantAddress", {
+    streetNum : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    street : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    aptNum : {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    State : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    zipCode : {
+        type: Sequelize.INT,
+        allowNull: false
+    }
+
+});
 module.exports = RestaurantAddress;
