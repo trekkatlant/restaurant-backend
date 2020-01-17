6f4d60db3cf5366c5c8f3a070d4a1a3ad5082c9c
Bobur <bobur.hu0991@gmail.com>
2020-01-17 14:52:34 -0500

    Closes #1

---
+++ database/models/userAddress.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const UserAddress = db.define("userAddress", {
+    streetNum : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    street : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    aptNum : {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    city : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    state : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    zipCode : {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = UserAddress;
