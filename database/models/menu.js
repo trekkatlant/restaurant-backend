fe62d774c4bfc1e2a911b6e09604055d718d4bbd
Bobur <bobur.hu0991@gmail.com>
2020-01-17 15:16:46 -0500

    Closes #3

---
+++ database/models/menu.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Menu = db.define("menuItem", {
+    item : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price : {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false
+    },
+    description : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl : {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Menu;
