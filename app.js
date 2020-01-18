d1ec3706118d0b86aa581a7d649eab3bb00514db
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-18 11:48:21 -0500

    edited app js

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 // const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const session = require("express-session");
+const passport = require("passport");
+const authRouter = require("./routes/auth");
+// const apiRouter = require("./routes");
+const cors = require("cors");
 const express = require("express");
 // const apiRouter = require("./routes/index");
-const app = express();
-const cors = require("cors");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sessionStore = new SequelizeStore({db});
 const db = require("./database");
 const seedDatabase = require("./seed/index");
+
+const app = express();
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try{
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  }
+  catch (err) {
+    done(err);
+  }
+});
+
 let PORT = process.env.PORT || 4000;
 app.use(logger('dev'));
 app.use(express.json());
