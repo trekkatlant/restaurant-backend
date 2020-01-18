53473d666865e415b208754ffa57b7d3f187442c
Bobur <bobur.hu0991@gmail.com>
2020-01-18 13:38:18 -0500

    seeding

--- seed/index.js
+++ seed/index.js
@@ -1,5 +1,15 @@
 const { User, UserAddress, PaymentInfo, Restaurant, 
 Menu, RestaurantAddress, Order, OrderItem } = require("../database/models");
+const items = require("./menu");
+
+const PopulateMenuTable = async (item) => {
+
+};
+const seedDatabase = async () => {
+
+};
+
+module.exports = seedDatabase;
 
 
 
