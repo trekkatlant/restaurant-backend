4038ba4e0a67ce4e8adcee8696fd932a468a6615
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-20 15:43:31 -0500

    user address route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const menuRoute = require("./menu");
 const userAddressRoute = require("./userAddress");
 router.use("/auth", authRoute);
 router.use("/menu", menuRoute);
-// router.use("/userAddress", userAddressRoute);
+router.use("/userAddress", userAddressRoute);
 
 router.get("/", (req, res, next) => {
   res.status(200).send("Default API route");
