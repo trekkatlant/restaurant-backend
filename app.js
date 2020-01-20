22f158c14369f8f726011b0e8859129deb2c3ffd
Bobur <bobur.hu0991@gmail.com>
2020-01-20 14:08:41 -0500

    working on auth

--- app.js
+++ app.js
@@ -39,24 +39,94 @@
 //   });
 // });
 
+//*****working app.js*****//
+
+
+// const express = require("express");
+// const apiRouter = require("./routes/index");
+// const app = express();
+// const cors = require("cors");
+// const db = require("./database");
+// const seedDatabase = require("./seed/index");
+// let PORT = process.env.PORT || 4000;
+
+// db.sync({force:true})
+// .then(async () => {
+//   seedDatabase();
+//   app.use(cors());
+//   app.get("/", (req, res, next) => {
+//     res.status(200).send("Nothing to see here");
+//   });
+//   app.use("/api", apiRouter);
+//   app.listen(PORT, () => {
+//     console.log("Server is listening on port" + PORT);
+//   })
+// });
+
 
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+// const authRouter = require("./routes/auth");
 const apiRouter = require("./routes/index");
-const app = express();
 const cors = require("cors");
-const db = require("./database");
-const seedDatabase = require("./seed/index");
-let PORT = process.env.PORT || 4000;
 
-db.sync({force:true})
-.then(async () => {
-  seedDatabase();
-  app.use(cors());
-  app.get("/", (req, res, next) => {
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const seedDatabase = require("./seed/index");
+const db = require("./database");
+const sessionStore = new SequelizeStore({ db });
+
+const app = express();
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    let user = await db.models.user.findByPk(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+const syncDb = async () => {
+  await db.sync({ force:true});
+};
+const configureApp = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended:true }));
+  app.use(cors({ credentials: true}));
+  app.use(
+    session({
+      secret: "SuperSecret",
+      store: sessionStore, 
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use("/api", apiRouter);
+};  
+const startListening = () => {
+  const PORT = process.env.PORT || 5000;
+  app.get("/", async(req, res, next) => {
     res.status(200).send("Nothing to see here");
   });
-  app.use("/api", apiRouter);
   app.listen(PORT, () => {
-    console.log("Server is listening on port" + PORT);
-  })
-});
+    console.log(`Listening on port ${PORT}`);
+  });
+};
+const bootApp = async () => {
+  await sessionStore.sync();
+  await syncDb();
+  await seedDatabase();
+  await configureApp();
+  await startListening();
+};
+
+//fires up everything
+bootApp();
+
+
+
