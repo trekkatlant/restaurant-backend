e09b2a01977ce03ef39b8cf3fab4c786159dfed0
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-22 14:13:23 -0500

    seeded paymentinfo

---
+++ seed/paymentInfo.js
@@ -0,0 +1,28 @@
+let payments = [
+    {
+        firstName : "Josh",
+        lastName : "Bosh",
+        cardNum : "123456789101123",
+        secCode : "666",
+        billStreetNum : "3232",
+        billStreet : "kings.St",
+        billAptNum : "5D",
+        billCity : "Rego Park",
+        billState : "NY",
+        billZip : "11111"
+    },
+    {
+        firstName : "Jim",
+        lastName : "Beam",
+        cardNum : "1001001011111100",
+        secCode : "123",
+        billStreetNum : "402",
+        billStreet : "Queens.St",
+        billAptNum : "A1",
+        billCity : "bang St",
+        billState : "NY",
+        billZip : "11001"
+    }
+];
+
+module.exports = payments;
