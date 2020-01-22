910d92cb975d26bca395c8adf23253280363326a
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-22 14:24:44 -0500

    seeded restaurantsAddress

---
+++ seed/restaurantAddress.js
@@ -0,0 +1,11 @@
+let restaurantAdds = [
+    {
+        streetNum : "1247",
+        street : "5th Ave",
+        city : "Rego Park",
+        billState : "NY",
+        billZip : "11111"
+    }
+];
+
+module.exports = restaurantAdds;
