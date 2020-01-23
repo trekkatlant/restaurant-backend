4f24abb204ff8519b3f25c81a32ac4a5e8b56cdd
Bobur <bobur.hu0991@gmail.com>
2020-01-22 23:02:55 -0500

    seeding

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const authRoute = require("./auth");
 const menuRoute = require("./menu");
 const userRoute = require("./user");
 const orderRoute = require("./order");
+const restaurantRoute = require("./restaurant");
+
 router.use("/auth", authRoute);
 router.use("/menu", menuRoute);
 router.use("/users", userRoute);
 router.use("/order", orderRoute);
+router.use("/restaurant", restaurantRoute);
 router.get("/", (req, res, next) => {
   res.status(200).send("Default API route");
 });
